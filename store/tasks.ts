61c720ad267e0a3377afa7816ee99c9fb77eef16
Kristina1901 <ktuzova1901@gmail.com>
2024-12-16 21:22:24 +0200

    fix

--- store/tasks.ts
+++ store/tasks.ts
@@ -1,75 +1,66 @@
-import { ref, computed } from "vue";
 import { defineStore } from "pinia";
 import { TaskStatus } from "../interfaces/task";
 import type { Task } from "../interfaces/task";
 
-export const useTasksStore = defineStore("tasks", () => {
-  const tasks = ref<{
-    [key in TaskStatus]: {
-      taskStatus: TaskStatus;
-      items: Task[];
-    };
-  }>({
-    [TaskStatus.TODO]: {
-      taskStatus: TaskStatus.TODO,
-      items: [],
+export const useTasksStore = defineStore("tasks", {
+  state: () => ({
+    tasks: {
+      [TaskStatus.TODO]: {
+        taskStatus: TaskStatus.TODO,
+        items: [] as Task[],
+      },
+      [TaskStatus.IN_PROGRESS]: {
+        taskStatus: TaskStatus.IN_PROGRESS,
+        items: [] as Task[],
+      },
+      [TaskStatus.DONE]: {
+        taskStatus: TaskStatus.DONE,
+        items: [] as Task[],
+      },
+    } as Record<TaskStatus, { taskStatus: TaskStatus; items: Task[] }>,
+  }),
+  getters: {
+    taskStatuses: () => Object.values(TaskStatus),
+    taskCountByStatus: (state) => {
+      return {
+        [TaskStatus.TODO]: state.tasks[TaskStatus.TODO].items.length,
+        [TaskStatus.IN_PROGRESS]:
+          state.tasks[TaskStatus.IN_PROGRESS].items.length,
+        [TaskStatus.DONE]: state.tasks[TaskStatus.DONE].items.length,
+      };
     },
-    [TaskStatus.IN_PROGRESS]: {
-      taskStatus: TaskStatus.IN_PROGRESS,
-      items: [],
+  },
+  actions: {
+    addTask(task: Task, taskStatus: TaskStatus) {
+      this.tasks[taskStatus].items.push(task);
     },
-    [TaskStatus.DONE]: {
-      taskStatus: TaskStatus.DONE,
-      items: [],
+    removeTask(taskId: string, taskStatus: TaskStatus) {
+      this.tasks[taskStatus].items = this.tasks[taskStatus].items.filter(
+        (task) => task.id !== taskId
+      );
     },
-  });
-  const taskStatuses = computed(() => Object.values(TaskStatus));
-  const taskCountByStatus = computed(() => {
-    return {
-      [TaskStatus.TODO]: tasks.value[TaskStatus.TODO].items.length,
-      [TaskStatus.IN_PROGRESS]:
-        tasks.value[TaskStatus.IN_PROGRESS].items.length,
-      [TaskStatus.DONE]: tasks.value[TaskStatus.DONE].items.length,
-    };
-  });
-  const addTask = (task: Task, taskStatus: TaskStatus) => {
-    tasks.value[taskStatus].items.push(task);
-  };
-  const removeTask = (taskId: string, taskStatus: TaskStatus) => {
-    tasks.value[taskStatus].items = tasks.value[taskStatus].items.filter(
-      (task) => task.id !== taskId
-    );
-  };
-  const updateTask = (updatedTask: Task) => {
-    const status = updatedTask.taskStatus;
-    const index = tasks.value[status].items.findIndex(
-      (task) => task.id === updatedTask.id
-    );
-    if (index !== -1) {
-      tasks.value[status].items.splice(index, 1, updatedTask);
-    }
-  };
-  const moveTask = (
-    taskId: string,
-    fromTaskStatus: TaskStatus,
-    toTaskStatus: TaskStatus
-  ) => {
-    if (fromTaskStatus === toTaskStatus) return;
-    const taskIndex = tasks.value[fromTaskStatus].items.findIndex(
-      (task) => task.id === taskId
-    );
-    if (taskIndex !== -1) {
-      const [task] = tasks.value[fromTaskStatus].items.splice(taskIndex, 1);
-      tasks.value[toTaskStatus].items.push(task);
-    }
-  };
-  return {
-    tasks,
-    taskStatuses,
-    taskCountByStatus,
-    addTask,
-    removeTask,
-    moveTask,
-    updateTask,
-  };
+    updateTask(updatedTask: Task) {
+      const status = updatedTask.taskStatus;
+      const index = this.tasks[status].items.findIndex(
+        (task) => task.id === updatedTask.id
+      );
+      if (index !== -1) {
+        this.tasks[status].items.splice(index, 1, updatedTask);
+      }
+    },
+    moveTask(
+      taskId: string,
+      fromTaskStatus: TaskStatus,
+      toTaskStatus: TaskStatus
+    ) {
+      if (fromTaskStatus === toTaskStatus) return;
+      const taskIndex = this.tasks[fromTaskStatus].items.findIndex(
+        (task) => task.id === taskId
+      );
+      if (taskIndex !== -1) {
+        const [task] = this.tasks[fromTaskStatus].items.splice(taskIndex, 1);
+        this.tasks[toTaskStatus].items.push(task);
+      }
+    },
+  },
 });
